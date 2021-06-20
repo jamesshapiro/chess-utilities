2e5ab871c34b8206f26571db45efdae30a6cd96e
James Shapiro <jamesshapiro@gmail.com>
2021-06-19 22:27:04 -0700

    add support for most moves aside from pawn promotion and castling status

--- moves_to_fen.py
+++ moves_to_fen.py
@@ -1,8 +1,11 @@
+import math
+import sys
+
 BOARD_FILES = 'abcdefgh'
 BOARD_RANKS = '87654321'
 
 to_move = 'w'
-board = [
+reference_board = [
     ['r','n','b','q','k','b','n','r'],
     ['p','p','p','p','p','p','p','p'],
     ['-','-','-','-','-','-','-','-'],
@@ -12,6 +15,27 @@ board = [
     ['P','P','P','P','P','P','P','P'],
     ['R','N','B','Q','K','B','N','R'],
 ]
+
+test_board = [
+    ['b','n','r','q','k','-','r','r'],
+    ['p','p','p','p','p','p','-','p'],
+    ['-','-','-','-','-','-','-','q'],
+    ['-','-','-','-','-','-','p','-'],
+    ['-','-','-','-','-','-','-','-'],
+    ['-','-','-','-','K','-','-','-'],
+    ['P','P','P','P','P','P','p','P'],
+    ['R','N','B','Q','R','B','N','R'],
+]
+
+
+def clone_board(board):
+    new_board = []
+    for row in board:
+        new_board.append(row[:])
+    return new_board
+
+board = clone_board(reference_board)
+
 castling_rights = 'KQkq'
 
 pawn_forward_moves = [
@@ -32,16 +56,12 @@ pawn_forward_promotion_moves = [
 ]
 
 
-def get_index_from_square(square):
+def get_coords_from_square(square):
     square_file = BOARD_FILES.index(square[0])
     square_rank = BOARD_RANKS.index(square[1])
     return square_file, square_rank
 
-def get_piece_from_square(square, board):
-    index_file, index_rank = get_index_from_square(square)
-    return board[index_rank][index_file]
-
-def get_piece_from_index(index, board):
+def get_piece_from_coords(board, index):
     index_file, index_rank = index
     return board[index_rank][index_file]
 
@@ -51,19 +71,35 @@ def index_to_square(coords):
     square_file = BOARD_FILES[index_file]
     return f'{square_file}{square_rank}'
 
-def clear_coords(square, board):
-    square_file, square_rank = square
+def clear_coords(board, coords):
+    square_file, square_rank = coords
     board[square_rank][square_file] = '-'
 
-def fill_coords(square, board, piece):
+def fill_coords(board, square, piece):
     square_file, square_rank = square
     board[square_rank][square_file] = piece
 
+icon_table = {
+    'K': '♔',
+    'Q': '♕',
+    'R': '♖',
+    'B': '♗',
+    'N': '♘',
+    'P': '♙',
+    'k': '♚',
+    'q': '♛',
+    'r': '♜',
+    'b': '♝',
+    'n': '♞',
+    'p': '♟'
+}
+    
 def print_board(board):
     for row in board:
-        print(row)
+        #print_row = [icon_table.get(piece, piece) for piece in row]
+        print(''.join(row))
 
-def pawn_is_taking(move):
+def pawn_is_taking_without_promoting(move):
     if len(move) != 4:
         return False
     if move[0] not in BOARD_FILES:
@@ -80,59 +116,337 @@ def pawn_is_moving_forward_without_promoting(move):
 def is_pawn_move(move):
     return any([
         pawn_is_moving_forward_without_promoting(move),
-        pawn_is_taking(move)        
+        pawn_is_taking_without_promoting(move)        
     ])
 
 def get_end_square(move):
-    return move[2:] if 'x' in move else move
+    move = move.replace('+','')
+    if '=' not in move:
+        return move[-2:]
+    print('TODO: implement promotion')
+    sys.exit(0)
 
-def get_pawn_start_coords(move, board, player):
+def get_pawn_start_coords(board, move, player):
+    move = move.replace('+','')
     end_square = get_end_square(move)
-    end_coords_file, end_coords_rank = get_index_from_square(end_square)
+    end_coords_file, end_coords_rank = get_coords_from_square(end_square)
     preceding_rank = end_coords_rank + 1 if player == 'w' else end_coords_rank - 1
     if pawn_is_moving_forward_without_promoting(move):
         preceding_file = end_coords_file
-        if get_piece_from_index((preceding_file, preceding_rank),board).lower() != 'p':
+        if get_piece_from_coords(board, (preceding_file, preceding_rank)).lower() != 'p':
             preceding_rank = end_coords_rank + 2 if player == 'w' else end_coords_rank - 2
         return preceding_file, preceding_rank
-    elif pawn_is_taking(move):
+    elif pawn_is_taking_without_promoting(move):
         preceding_file = BOARD_FILES.index(move[0])
         return preceding_file, preceding_rank
 
-def clear_en_passant_coords_if_necessary(move, board, player):
+def clear_en_passant_coords_if_necessary(board, move, player):
     end_square = get_end_square(move)
-    end_coords = get_index_from_square(end_square)
+    end_coords = get_coords_from_square(end_square)
     end_coords_file, end_coords_rank = end_coords
     en_passant_rank = end_coords_rank + 1 if player == 'w' else end_coords_rank - 1
     en_passant_coords = (end_coords_file, en_passant_rank)
-    if get_piece_from_index(end_coords, board) == '-':
-        clear_coords(en_passant_coords, board)
+    if get_piece_from_coords(board, end_coords) == '-':
+        clear_coords(board, en_passant_coords)
+
+def get_piece_coords(board, piece, color):
+    all_coords = []
+    piece_key = piece.upper() if color == 'w' else piece.lower()
+    for rank_index, rank_row in enumerate(board):
+        for file_index, square in enumerate(rank_row):
+            if square == piece_key:
+                all_coords.append((file_index, rank_index))
+    return all_coords
+
+def get_king_coords(board, player):
+    return get_piece_coords(board, 'k', player)[0]
+
+def get_bishop_coords(board, color):
+    return get_piece_coords(board, 'b', color)
+
+def get_knight_coords(board, color):
+    return get_piece_coords(board, 'n', color)
+
+def get_rook_coords(board, color):
+    return get_piece_coords(board, 'r', color)
+
+def get_queen_coords(board, color):
+    return get_piece_coords(board, 'q', color)
+
+def get_pawn_coords(board, color):
+    return get_piece_coords(board, 'p', color)
+
+def get_radius(coords, candidate_deltas):
+    coords_file, coords_rank = coords
+    candidate_squares = [(coords_file + x, coords_rank + y) for x,y in candidate_deltas]
+    candidate_squares = [(x,y) for x,y in candidate_squares if x >= 0 and x < 8 and y >= 0 and y < 8]
+    return candidate_squares
     
-def process_move(move, board, player):
-    end_coords = (-1,-1)
+
+def get_knight_radius(coords):
+    candidate_deltas = [(1,2),(2,1),(1,-2),(-2,1),(-1,2),(2,-1),(-1,-2),(-2,-1)]
+    return get_radius(coords, candidate_deltas)
+
+def get_bishop_radius(coords):
+    candidate_deltas = [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)]
+    negative   = [(-x, -y) for x,y in candidate_deltas]
+    negative_x = [(-x,  y) for x,y in candidate_deltas]
+    negative_y = [( x, -y) for x,y in candidate_deltas]
+    candidate_deltas = [*candidate_deltas, *negative, *negative_x, *negative_y]
+    return get_radius(coords, candidate_deltas)
+
+def get_rook_radius(coords):
+    candidate_deltas = [(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)]
+    negative   = [(-x, y) for x,y in candidate_deltas]
+    candidate_deltas = [*candidate_deltas, *negative]
+    flipped    = [(y,x)   for x,y in candidate_deltas]
+    candidate_deltas = [*candidate_deltas, *flipped]
+    return get_radius(coords, candidate_deltas)
+
+def get_queen_radius(coords):
+    bishop_radius = get_bishop_radius(coords)
+    rook_radius = get_rook_radius(coords)
+    return [*bishop_radius, *rook_radius]
+
+def get_pawn_radius(coords, color):
+    direction = -1 if color == 'w' else 1
+    candidate_deltas = [(-1,direction), (1, direction)]
+    return get_radius(coords, candidate_deltas)
+
+def get_king_radius(coords):
+    candidate_deltas = [
+        (-1,-1),(0,-1),(1,-1),
+        (-1,0),        (1,0),
+        (-1,1), (0, 1),(1,1)
+    ]
+    return get_radius(coords, candidate_deltas)
+
+def bishop_can_reach_square(board, bishop_coords, square_coords):
+    bishop_file, bishop_rank = bishop_coords
+    square_file, square_rank = square_coords
+    file_difference = bishop_file - square_file
+    rank_difference = bishop_rank - square_rank
+    if abs(file_difference) != abs(rank_difference):
+        return False
+    rank_direction = int(math.copysign(1,rank_difference))
+    file_direction = int(math.copysign(1,file_difference))
+    for i in range(1, abs(rank_difference)):
+        new_coords = (bishop_file - (i * file_direction), bishop_rank - (i * rank_direction))
+        if get_piece_from_coords(board, new_coords) != '-':
+            return False
+    return True
+
+def bishop_checks_king(board, bishop_coords, king_coords):
+    return bishop_can_reach_square(board, bishop_coords, king_coords)
+
+def rook_can_reach_square(board, rook_coords, square_coords):
+    rook_file, rook_rank = rook_coords
+    square_file, square_rank = square_coords
+    file_difference = rook_file - square_file
+    rank_difference = rook_rank - square_rank
+    if min(abs(file_difference), abs(rank_difference)) != 0:
+        return False
+    rook_is_same_rank = square_rank == rook_rank
+    direction = int(math.copysign(1, rank_difference)) 
+    if rook_is_same_rank:
+        direction = int(math.copysign(1, file_difference))
+    for i in range(1, max(abs(file_difference), abs(rank_difference))):
+        file_increment = direction if rook_is_same_rank else 0
+        rank_increment = direction if not rook_is_same_rank else 0
+        new_coords = (rook_file - (i * file_increment), rook_rank - (i * rank_increment))
+        if get_piece_from_coords(board, new_coords) != '-':
+            return False
+    return True
+    
+def rook_checks_king(board, rook_coords, king_coords):
+    return rook_can_reach_square(board, rook_coords, king_coords)
+
+def queen_can_reach_square(board, queen_coords, square_coords):
+    return any([
+        bishop_can_reach_square(board, queen_coords, square_coords),
+        rook_can_reach_square(board, queen_coords, square_coords)
+    ])
+    
+def queen_checks_king(board, queen_coords, king_coords):
+    return queen_can_reach_square(board, queen_coords, king_coords)
+
+def king_is_in_check(board, player):
+    player_king = 'K' if player == 'w' else 'k'
+    player_king_coords = get_king_coords(board, player)
+    opp_color = 'b' if player == 'w' else 'w'
+    opp_pawn_coords = get_pawn_coords(board, opp_color)
+    opp_knight_coords = get_knight_coords(board, opp_color)
+    opp_king_coords = get_king_coords(board, opp_color)
+    opp_bishop_coords = get_bishop_coords(board, opp_color)
+    opp_rook_coords = get_rook_coords(board, opp_color)
+    opp_queen_coords = get_queen_coords(board, opp_color)
+    unsafe_squares = []
+    for pawn_coords in opp_pawn_coords:
+        unsafe_squares.extend(get_pawn_radius(pawn_coords, opp_color))
+    for knight_coords in opp_knight_coords:
+        unsafe_squares.extend(get_knight_radius(knight_coords))
+    unsafe_squares.extend(get_king_radius(opp_king_coords))
+    for unsafe_square in unsafe_squares:
+        if get_piece_from_coords(board, unsafe_square) == player_king:
+            return True
+    for bishop_coords in opp_bishop_coords:
+        if bishop_checks_king(board, bishop_coords, player_king_coords):
+            return True
+    for rook_coords in opp_rook_coords:
+        if rook_checks_king(board, rook_coords, player_king_coords):
+            return True
+    for queen_coords in opp_queen_coords:
+        if queen_checks_king(board, queen_coords, player_king_coords):
+            return True
+    return False
+
+def move_piece(board, start_coords, end_coords, piece):
+    clear_coords(board, start_coords)
+    fill_coords(board, end_coords, piece)
+
+def get_knight_start_coords(board, move, end_coords, player, piece):
+    move = move.replace('x','')
+    move = move.replace('+','')
+    if len(move) == 5:
+        return get_coords_from_square(move[1:3])
+    knight_radius = get_knight_radius(end_coords)
+    knight_coords = get_knight_coords(board, player)
+    knight_candidates = list(set(knight_radius) & set(knight_coords))
+    if len(move) == 4:
+        specifier = move[1]
+        if specifier in BOARD_FILES:
+            specified_file = BOARD_FILES.index(specifier)
+            knight_candidates = [candidate for candidate in knight_candidates if candidate[0] == specified_file]
+        else:
+            specified_rank = BOARD_RANKS.index(specifier)
+            knight_candidates = [candidate for candidate in knight_candidates if candidate[1] == specified_rank]
+    for candidate in knight_candidates:
+        board_clone = clone_board(board)
+        move_piece(board_clone, candidate, end_coords, piece)
+        if not king_is_in_check(board_clone, player):
+            return candidate
+
+def get_brq_start_coords(board, move, end_coords, get_radius_fn, brq_can_reach_fn, player, piece):
+    move = move.replace('x','')
+    move = move.replace('+','')
+    if len(move) == 5:
+        return get_coords_from_square(move[1:3])
+    brq_radius = get_radius_fn(end_coords)
+    brq_coords = get_piece_coords(board, piece, player)
+    brq_candidates = list(set(brq_radius) & set(brq_coords))
+    if len(move) == 4:
+        specifier = move[1]
+        if specifier in BOARD_FILES:
+            specified_file = BOARD_FILES.index(specifier)
+            brq_candidates = [candidate for candidate in brq_candidates if candidate[0] == specified_file]
+        else:
+            specified_rank = BOARD_RANKS.index(specifier)
+            brq_candidates = [candidate for candidate in brq_candidates if candidate[1] == specified_rank]
+    brq_candidates = list(filter(lambda x: brq_can_reach_fn(board, x, end_coords), brq_candidates))
+    for candidate in brq_candidates:
+        board_clone = clone_board(board)
+        move_piece(board_clone, candidate, end_coords, piece)
+        if not king_is_in_check(board_clone, player):
+            return candidate
+
+def get_bishop_start_coords(board, move, end_coords, player, piece):
+    return get_brq_start_coords(
+        board, move, end_coords, get_bishop_radius,
+        bishop_can_reach_square, player, piece
+    )
+    
+def get_rook_start_coords(board, move, end_coords, player, piece):
+    return get_brq_start_coords(
+        board, move, end_coords, get_rook_radius,
+        rook_can_reach_square, player, piece
+    )
+    
+def get_queen_start_coords(board, move, end_coords, player, piece):
+    return get_brq_start_coords(
+        board, move, end_coords, get_queen_radius,
+        queen_can_reach_square, player, piece
+    )
+
+move_fn_map = {
+    'N': get_knight_start_coords,
+    'B': get_bishop_start_coords,
+    'R': get_rook_start_coords,
+    'Q': get_queen_start_coords
+}
+
+def castle(board, player, is_kingside):
+    king = 'K' if player == 'w' else 'k'
+    rook = 'R' if player == 'w' else 'r'
+    rank = BOARD_RANKS.index('1') if player == 'w' else BOARD_RANKS.index('8')
+    clear_castle_file = 'h' if is_kingside else 'a'
+    new_king_file = 'g' if is_kingside else 'c'
+    new_castle_file = 'f' if is_kingside else 'd'
+    clear_coords(board, (BOARD_FILES.index('e'), rank))
+    clear_coords(board, (BOARD_FILES.index(clear_castle_file), rank))
+    fill_coords(board, (BOARD_FILES.index(new_king_file), rank), king)
+    fill_coords(board, (BOARD_FILES.index(new_castle_file), rank), rook)
+
+def is_castles(move):
+    return move in ['O-O','O-O-O']
+
+def is_king_move(move):
+    return move[0] == 'K'
+    
+def process_move(board, move, player):
+    if is_castles(move):
+        if move == 'O-O':
+            castle(board, player, is_kingside=True)
+        else:
+            castle(board, player, is_kingside=False)
+        return
+    end_square = get_end_square(move)
+    end_coords = get_coords_from_square(end_square)
     start_coords = (-1,-1)
-    piece = '-'
+    piece = move[0].upper() if player == 'w' else move[0].lower()
     if is_pawn_move(move):
         piece = 'P' if player == 'w' else 'p'
-        start_coords = get_pawn_start_coords(move, board, player)
-        print(start_coords)
-    if pawn_is_moving_forward_without_promoting(move):
-        end_coords = get_index_from_square(move)
-    elif pawn_is_taking(move):
-        print('PAWN IS TAKING')
-        end_square = get_end_square(move)
-        end_coords = get_index_from_square(end_square)
-        clear_en_passant_coords_if_necessary(move, board, player)
-    clear_coords(start_coords, board)
-    fill_coords(end_coords, board, piece)
-    print()
+        start_coords = get_pawn_start_coords(board, move, player)
+        if pawn_is_taking_without_promoting(move):
+            clear_en_passant_coords_if_necessary(board, move, player)
+    elif move[0] in move_fn_map:
+        start_coords = move_fn_map[move[0]](board, move, end_coords, player, piece)
+    elif is_king_move(move):
+        start_coords = get_king_coords(board, player)
+
+    move_piece(board, start_coords, end_coords, piece)
     print_board(board)
     pass
 
 print_board(board)
 
-moves = ['e4', 'e5', 'd4', 'exd4','c4','dxc3']
+#moves = ['e4', 'e5', 'd4', 'exd4','Nc3','dxc3', 'Bc4']
+moves = ["e4", "d5", "exd5", "Qxd5", "Qf3", "Qxf3", "Nxf3", "Nf6", "Bc4", "Nc6", "d3", "Bf5", "Nc3", "O-O-O", "O-O", "Na5", "Bb3", "Nxb3", "axb3", "Kb8", "Be3", "b6", "Nb5", "a5", "c3", "Bc8", "b4", "Ba6", "c4", "Bxb5", "cxb5", "Rxd3", "bxa5", "bxa5", "Rxa5", "Rb3", "Rfa1", "Kc8", "Bd4", "e6", "Bc3", "Bd6", "Nd4", "Kd7", "Nxb3", "Nd5", "Nd4", "Nxc3", "bxc3", "Ke7", "c4", "Kf6", "Nc6", "Bc5", "Ra6", "Bb6", "R6a4", "Bc5", "Rc1", "Re8", "Rd1", "e5", "Nd8", "Bd6", "Nc6", "Bc5", "Rd5", "Bb6", "Ra6", "Bd4", "Nxd4+", "Ke7", "Nf3"]
 
 for idx, move in enumerate(moves):
-    process_move(move, board, 'w' if idx % 2 == 0 else 'b')
+    print(f'({idx}.) {move}')
+    process_move(board, move, 'w' if idx % 2 == 0 else 'b')
+    print()
 
+"""
+print()
+print()
+print()
+
+
+print_board(test_board)
+
+player_king_coords = get_king_coords(test_board, 'w')
+opp_rook_coords = get_rook_coords(test_board, 'b')
+for rook_coords in opp_rook_coords:
+    print(f'{rook_checks_king(test_board, rook_coords, player_king_coords)=}')
+
+print('\n========================================\n')    
+    
+player_king_coords = get_king_coords(test_board, 'w')
+opp_bishop_coords = get_bishop_coords(test_board, 'b')
+for bishop_coords in opp_bishop_coords:
+    print(f'{bishop_checks_king(test_board, bishop_coords, player_king_coords)=}')
+
+print('\n========================================\n')    
+print(king_is_in_check(test_board, 'w'))
+"""
